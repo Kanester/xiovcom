995c0a6643896a54630655496fc7db27873a059f
Xiöv <pampagkieven091@gmail.com>
2025-07-18 14:32:02 +0800

    init analytics

---
+++ src/routes/+layout.ts
@@ -0,0 +1,5 @@
+import { dev } from '$app/environment';
+import { injectAnalytics } from '@vercel/analytics/sveltekit';
+
+injectAnalytics({ mode: dev ? 'development' : 'production' });
+if (dev) import('eruda').then(res => res.init());
