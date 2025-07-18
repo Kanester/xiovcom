fb3290e341b9a3a2e78877dbb0ca1c3ebbbbebcc
Xiöv <pampagkieven091@gmail.com>
2025-07-18 21:43:56 +0800

    fixed navbar for desktop

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-import Kofi from "$component/Kofi.svelte";
-import Navbar from "$component/Navbar.svelte";
+	import Kofi from '$component/Kofi.svelte';
+	import Navbar from '$component/Navbar.svelte';
 	const { children } = $props();
 </script>
 
@@ -18,56 +18,67 @@ import Navbar from "$component/Navbar.svelte";
 		<li><a href="/">Home</a></li>
 		<li><a href="/blog">Blog</a></li>
 		<li><a href="/projects">Projects</a></li>
+		<li><a href="/contact">Contact</a></li>
 	</ul>
 	<Kofi />
 </footer>
 
 <style lang="scss">
-@use "$styles/variables" as *;
-:global {
-	*, *::before, *::after {
-		margin: 0;
-		padding: 0;
-		box-sizing: border-box;
+	@use '$styles/variables' as *;
+	:global {
+		*,
+		*::before,
+		*::after {
+			margin: 0;
+			padding: 0;
+			box-sizing: border-box;
+		}
+
+		html,
+		body {
+			width: 100%;
+			height: 100%;
+			scroll-behavior: smooth;
+			font-family: system-ui, sans-serif;
+			background-color: $primary-bg;
+			color: $text-color;
+		}
+
+		body {
+			line-height: 1.5;
+			-webkit-font-smoothing: antialiased;
+			-moz-osx-font-smoothing: grayscale;
+		}
+
+		img,
+		picture,
+		video,
+		canvas,
+		svg {
+			display: block;
+			max-width: 100%;
+		}
+
+		input,
+		button,
+		textarea,
+		select {
+			font: inherit;
+		}
+
+		a {
+			color: inherit;
+			text-decoration: none;
+		}
+
+		ul {
+			list-style: none;
+			padding: 0;
+			margin: 0;
+		}
 	}
 
-	html, body {
-		width: 100%;
-		height: 100%;
-		scroll-behavior: smooth;
-		font-family: system-ui, sans-serif;
-		background-color: $primary-bg;
-		color: $text-color;
+	footer {
+		background-color: $secondary-bg;
 	}
-
-	body {
-		line-height: 1.5;
-		-webkit-font-smoothing: antialiased;
-		-moz-osx-font-smoothing: grayscale;
-	}
-
-	img, picture, video, canvas, svg {
-		display: block;
-		max-width: 100%;
-	}
-
-	input, button, textarea, select {
-		font: inherit;
-	}
-
-	a {
-		color: inherit;
-		text-decoration: none;
-	}
-
-	ul {
-		list-style: none;
-		padding: 0;
-		margin: 0;
-	}
-}
-
-footer {
-	background-color: $secondary-bg;
-}
 </style>
