966dbf3526750406e096fc28bea90f783ead3f7e
Xiöv <pampagkieven091@gmail.com>
2025-09-28 18:44:10 +0800

    last

--- src/assets/interface/forms/SignOut.svelte
+++ src/assets/interface/forms/SignOut.svelte
@@ -6,6 +6,6 @@
     <h2>Log Out</h2>
     <p>Are you sure you want to log out?</p>
     <div class="actions">
-      <button class="cancel" on:click|preventDefault={() => showAuthModal.set(false)}>Cancel</button>
-      <button class="logout" on:click|preventDefault={() => logout}>Log Out</button>
+      <button on:click|preventDefault={() => showAuthModal.set(false)}>Cancel</button>
+      <button on:click|preventDefault={logout}>Log Out</button>
     </div>
