6324386950747584555ac34488929e2dbb655b1f
Xiöv <pampagkieven091@gmail.com>
2025-09-28 18:49:05 +0800

    last ladt alet

--- src/assets/interface/forms/SignOut.svelte
+++ src/assets/interface/forms/SignOut.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
 	import { logout } from "$lib/services/firebase";
-	import { showAuthModal } from "$stores/auth";
+	import { showSignOut } from "$stores/auth";
+	
+	const handleSignOut = () => {
+		logout();
+		showSignOut.set(false);
+	}
 </script>
 
     <h2>Log Out</h2>
     <p>Are you sure you want to log out?</p>
     <div class="actions">
-      <button on:click|preventDefault={() => showAuthModal.set(false)}>Cancel</button>
-      <button on:click|preventDefault={logout}>Log Out</button>
+      <button on:click={() => showSignOut.set(false)}>Cancel</button>
+      <button on:click={handleSignOut}>Log Out</button>
     </div>
