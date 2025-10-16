f2d82607832bc74bea967f34707296918f8ebc27
Kanester <pampagkieven091@gmail.com>
2025-10-16 18:49:49 +0800

    eh??

--- src/stores/user.ts
+++
@@ -1,9 +0,0 @@
-import { writable } from 'svelte/store';
-import { auth } from '$lib/services/firebase';
-import { onAuthStateChanged, type User } from 'firebase/auth';
-
-export const user = writable(auth.currentUser);
-
-onAuthStateChanged(auth, (firebaseUser) => {
-	user.set(firebaseUser);
-});
